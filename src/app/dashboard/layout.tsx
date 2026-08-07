6135e7ee657a6afb000d1e5bbf9062cdf3de02ee
rafidhp <rafiislamipasha27@gmail.com>
2026-08-08 02:50:21 +0700

    fix: minor

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,4 @@
-import "@/styles/globals.css";
-
 import { type Metadata } from "next";
-import { Poppins } from "next/font/google";
 import AppLayout from "@/layouts/app-layout";
 import type { BreadcrumbItem } from "@/layouts/app-layout";
 
@@ -17,32 +14,12 @@ export const metadata: Metadata = {
   description: "KacamataKoe admin page",
 };
 
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: [
-    "100",
-    "200",
-    "300",
-    "400",
-    "500",
-    "600",
-    "700",
-    "800",
-    "900",
-  ],
-  variable: "--font-poppins",
-});
-
 export default function DashboardLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${poppins.variable}`}>
-      <body className="font-sans">
-        <AppLayout breadcrumbs={breadcrumbs}>
-          {children}
-        </AppLayout>
-      </body>
-    </html>
+    <AppLayout breadcrumbs={breadcrumbs}>
+      {children}
+    </AppLayout>
   );
 }
